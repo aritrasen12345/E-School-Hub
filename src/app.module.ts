50882cb7affe452815cc1c31367dd99a1ac46840
Aritra Sen <aritrasen12345@gmail.com>
2024-06-01 15:10:46 +0530

    added cronjob for backend url health-check and implemented swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TasksModule } from './modules/tasks/tasks.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    // * Import config module
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+
+    // * Import TaskModule
+    TasksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
