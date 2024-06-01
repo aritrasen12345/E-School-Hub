3cc4aece3178c8b7bfa9995200ae7ac4d1173a8d
Aritra Sen <aritrasen12345@gmail.com>
2024-06-01 13:03:37 +0530

    add cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // * enable cors in the project
+  app.enableCors({ origin: '*' });
+
   await app.listen(3000);
 }
 bootstrap();
