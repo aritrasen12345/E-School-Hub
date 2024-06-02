88a0d22ed2c295561cf4c43b4c352e98bd2ae5dc
Aritra Sen <aritrasen12345@gmail.com>
2024-06-02 07:58:59 +0530

    defined school and student schema

---
+++ src/common/schemas/school.schema.ts
@@ -0,0 +1,57 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema({
+  versionKey: false,
+  timestamps: true,
+  toObject: {
+    virtuals: true,
+    transform: function (_, ret) {
+      delete ret.id;
+    },
+  },
+  toJSON: {
+    virtuals: true,
+    transform: function (_, ret) {
+      delete ret.id;
+    },
+  },
+})
+export class School extends Document {
+  @Prop({
+    type: String,
+    required: true,
+  })
+  email: string;
+
+  @Prop({
+    type: String,
+    required: true,
+  })
+  password: string;
+
+  @Prop({
+    type: String,
+    required: true,
+  })
+  schoolName: string;
+
+  @Prop({
+    type: Boolean,
+    default: false,
+  })
+  isDeleted: boolean;
+
+  @Prop({
+    type: Boolean,
+    default: false,
+  })
+  isVerified: boolean;
+
+  @Prop({
+    type: String,
+  })
+  verifyToken: string;
+}
+
+export const SchoolSchema = SchemaFactory.createForClass(School);
--- src/common/schemas/student.schema.ts
+++ src/common/schemas/student.schema.ts
@@ -1,11 +1,29 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { ApiProperty } from '@nestjs/swagger';
-import { Document } from 'mongoose';
+import { Document, Schema as MongooseSchema } from 'mongoose';
+import { School } from './school.schema';
 
-@Schema({ versionKey: false, timestamps: true })
+@Schema({
+  versionKey: false,
+  timestamps: true,
+  toObject: {
+    virtuals: true,
+    transform: function (_, ret) {
+      delete ret.id;
+    },
+  },
+  toJSON: {
+    virtuals: true,
+    transform: function (_, ret) {
+      delete ret.id;
+    },
+  },
+})
 export class Student extends Document {
   @ApiProperty({
     type: String,
+    required: true,
+    description: 'student name property!',
   })
   @Prop({
     type: String,
@@ -14,16 +32,77 @@ export class Student extends Document {
   })
   name: string;
 
-  // parentName
-  // gender
-  //standard
-  // section
-  // roll
-  // mobileNo
-  // address
-  // bloodGroup
-  // isDeleted
-  // schoolId
+  @Prop({
+    type: String,
+    required: true,
+    trim: true,
+  })
+  parentName: string;
+
+  @Prop({
+    type: String,
+    required: true,
+  })
+  gender: string;
+
+  @Prop({
+    type: String,
+    required: true,
+  })
+  standard: string;
+
+  @Prop({
+    type: String,
+    required: true,
+  })
+  section: true;
+
+  @Prop({
+    type: String,
+    required: true,
+  })
+  roll: string;
+
+  @Prop({
+    type: String,
+    required: true,
+  })
+  mobileNo: string;
+
+  @Prop({
+    type: String,
+    required: true,
+    trim: true,
+  })
+  address: string;
+
+  @Prop({
+    type: String,
+    required: true,
+  })
+  bloodGroup: string;
+
+  @Prop({
+    type: Boolean,
+    required: true,
+  })
+  isDeleted: boolean;
+
+  @Prop({
+    type: MongooseSchema.Types.ObjectId,
+    ref: School.name,
+    required: true,
+  })
+  schoolId: string;
+
+  school?: School;
 }
 
 export const StudentSchema = SchemaFactory.createForClass(Student);
+
+StudentSchema.virtual('school', {
+  ref: Student.name,
+  localField: 'schoolId',
+  foreignField: '_id',
+  justOne: true,
+});
