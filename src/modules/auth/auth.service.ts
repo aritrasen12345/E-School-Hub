bf04f68b09e86f74b317eae54ffe63af6f4b9a62
Aritra Sen <aritrasen12345@gmail.com>
2024-06-29 19:49:56 +0530

    Rename login method to generateToken. Also added validateSchoolCredentials

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,3 +1,4 @@
+// * PACKAGE IMPORTS
 import {
   Injectable,
   Logger,
@@ -7,8 +8,10 @@ import {
 import * as bcrypt from 'bcrypt';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+// * LOCAL IMPORTS
 import { School } from 'src/common/schemas';
 import { AuthHelper } from './helpers/auth.helper';
+import { SchoolDocument } from 'src/common/types';
 
 @Injectable()
 export class AuthService {
@@ -19,17 +22,35 @@ export class AuthService {
     private readonly authHelper: AuthHelper,
   ) {}
 
-  async login(
-    email: string,
-    password: string,
+  // * METHOD FOR GENERATING ACCESS AND REFRESH TOKENS
+  async generateToken(
+    schoolId: string,
     ipAddress: string,
   ): Promise<{ accessToken: string; refreshToken: string }> {
+    // * GENERATE ACCESS_TOKEN AND REFRESH_TOKEN
+    const { accessToken, refreshToken } = await this.authHelper.generateTokens(
+      schoolId,
+      ipAddress,
+    );
+
+    // * IF EVERYTHING WAS SUCCESSFUL
+    return { accessToken, refreshToken };
+  }
+
+  // * METHOD FOR VALIDATING SCHOOL LOGIN CREDENTIALS
+  async validateSchoolCredentials(
+    email: string,
+    password: string,
+  ): Promise<SchoolDocument> {
+    this.logger.debug('Inside validateSchoolCredentials!');
+
     // * FIRST CHECKING WHETHER THE SCHOOL IS REGISTERED OR NOT
     const foundSchool = await this.schoolModel.findOne({
       email,
       isVerified: true,
     });
 
+    // * IF SCHOOL IS NOT FOUND
     if (!foundSchool) {
       throw new NotFoundException('No school found with the given email!');
     }
@@ -38,11 +59,13 @@ export class AuthService {
     if (!foundSchool.isVerified) {
       throw new UnauthorizedException('Please verify your email to login!');
     }
+
     // * COMPARING PASSWORD
     const isPasswordMatched = await bcrypt.compare(
       password,
       foundSchool.password,
     );
+
     // * IF THE PASSWORD IS WRONG
     if (!isPasswordMatched) {
       throw new UnauthorizedException(
@@ -50,13 +73,7 @@ export class AuthService {
       );
     }
 
-    // * GENERATE ACCESS_TOKEN AND REFRESH_TOKEN
-    const { accessToken, refreshToken } = await this.authHelper.generateTokens(
-      foundSchool.id,
-      ipAddress,
-    );
-
     // * IF EVERYTHING WAS SUCCESSFUL
-    return { accessToken, refreshToken };
+    return foundSchool;
   }
 }
