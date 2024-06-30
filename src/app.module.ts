57fbc684a9395fe5763e5b50e906b83bc086087a
Aritra Sen <aritrasen12345@gmail.com>
2024-06-30 15:37:46 +0530

    added all_exception_filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,8 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { mongooseAsyncConfig } from './config/db/mongoose.config';
 import { StudentModule } from './modules/student/student.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { APP_FILTER } from '@nestjs/core';
+import { AllExceptionFilter } from './common/filters';
 
 @Module({
   imports: [
@@ -28,6 +30,12 @@ import { AuthModule } from './modules/auth/auth.module';
     AuthModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionFilter,
+    },
+    AppService,
+  ],
 })
 export class AppModule {}
