c0b17a0aac43b34d50d350b90f1358be787d9408
Aritra Sen <aritrasen12345@gmail.com>
2024-07-03 02:05:20 +0530

    Added skip_response_interceptor decorator

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 import { AppService } from './app.service';
+import { SkipResponseInterceptor } from './common/decorators';
 
 @Controller()
 export class AppController {
+  private readonly logger = new Logger(AppController.name);
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @SkipResponseInterceptor()
   getHello(): string {
+    this.logger.debug('Inside getHello!');
     return this.appService.getHello();
   }
 }
---
+++ src/common/decorators/skip_response_interceptor.decorator.ts
@@ -0,0 +1,4 @@
+import { SetMetadata } from '@nestjs/common';
+
+export const SkipResponseInterceptor = () =>
+  SetMetadata('skipResponseInterceptor', true);
